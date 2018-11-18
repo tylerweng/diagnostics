6be9c3f38607ac1942155048cadfc5be3d1db001
Tyler Weng <tylercweng@gmail.com>
2018-11-17 18:18:20 -0800

    express

--- index.js
+++ index.js
@@ -6,29 +6,37 @@ const {
 
 const {createClient, setVal, getVal, exitClient} = require('./database.js')
 
+const express = require('express')
+
 // setVal(redisClient, "kappa", 1)
 // setVal(redisClient, "keepo", 1)
 // exitClient(redisClient)
 
 // Create an app instance
 const redisClient = createClient();
-const app = actionssdk()
 
 // Register handlers for Actions SDK intents
 
-app.intent('actions.intent.MAIN', conv => {
-  conv.ask('Welcome to diagnostics!')
-  conv.ask('Here are a list of actions you may take: how many, bye, cancel')
-})
-
-app.intent('actions.intent.TEXT', (conv, input) => {
-  if (input === 'bye' || input === 'cancel') {
-    return conv.close('See you later!')
-  } else if (input === 'how many?') {
-    const numberOfLicks = getVal(redisClient, 'licks')
-    conv.ask(`You have ${numberOfLicks} licks`)
-    conv.ask('Any other commands?')
-  } else {
-    conv.ask(`I didn't understand. Can you tell me something else?`)
-  }
-})
+const PORT = process.env.PORT || 5000
+express()
+  .get('/', (req, res) => {
+    console.log(`Received GET req to /`)
+    const app = actionssdk()
+    app.intent('actions.intent.MAIN', conv => {
+      conv.ask('Welcome to diagnostics!')
+      conv.ask('Here are a list of actions you may take: how many, bye, cancel')
+    })
+    
+    app.intent('actions.intent.TEXT', (conv, input) => {
+      if (input === 'bye' || input === 'cancel') {
+        return conv.close('See you later!')
+      } else if (input === 'how many?') {
+        const numberOfLicks = getVal(redisClient, 'licks')
+        conv.ask(`You have ${numberOfLicks} licks`)
+        conv.ask('Any other commands?')
+      } else {
+        conv.ask(`I didn't understand. Can you tell me something else?`)
+      }
+    })
+  })
+  .listen(PORT, () => console.log(`Listening on port: ${PORT}`))
